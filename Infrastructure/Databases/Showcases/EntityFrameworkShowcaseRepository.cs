51a1279703e4de2e287943a300264a2cc945d0a4
bakersbakebread <hello@seanthorne.co.uk>
2022-11-12 14:35:15 +0000

    remove not needed injections

--- Infrastructure/Databases/Showcases/EntityFrameworkShowcaseRepository.cs
+++ Infrastructure/Databases/Showcases/EntityFrameworkShowcaseRepository.cs
@@ -3,29 +3,17 @@ namespace Service.Showcase.Infrastructure.Databases.Showcases;
 using Application.Showcase;
 using Application.Showcase.Entities;
 using AutoMapper;
-using Extensions;
 using Microsoft.EntityFrameworkCore;
-using SimpleDateTimeProvider;
 
 internal class EntityFrameworkShowcaseRepository : IShowcaseRepository
 {
     private readonly ShowcaseDbContext _context;
-    private readonly IDateTimeProvider _dateTimeProvider;
     private readonly IMapper _mapper;
 
-    public EntityFrameworkShowcaseRepository(ShowcaseDbContext context, IDateTimeProvider dateTimeProvider,
-        IMapper mapper, IHostEnvironment environment)
+    public EntityFrameworkShowcaseRepository(ShowcaseDbContext context, IMapper mapper)
     {
         _context = context;
-        _dateTimeProvider = dateTimeProvider;
         _mapper = mapper;
-
-        // if (!environment.IsDevelopment()) 
-        //     return;
-        //
-        // _ = _context.Database.EnsureCreated();
-        // _ = _context.AddData();
-
     }
 
     public virtual async Task<List<Showcase>> GetShowcases(CancellationToken cancellationToken)
